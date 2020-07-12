9fe088e1d31a5e4147e2310c8770142cd648926b
BetoSalcido <betho.salcidoo@gmail.com>
2020-07-11 19:53:10 -0500

    Apollo client configured

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react';
 import Head from 'next/head';
 import SideBar from '../components/Sidebar';
 import { useRouter } from 'next/router';
+import Header from './Header';
 
 const Layout = (props) => {
   const route = useRouter();
@@ -9,7 +10,7 @@ const Layout = (props) => {
   return (
     <Fragment>
       <Head>
-      <title>CRM - ADMIN</title>
+        <title>CRM - ADMIN</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" />
         <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"/>
       </Head>
